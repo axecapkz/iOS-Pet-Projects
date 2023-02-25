a63025ee4472596c92a175193ff5fa4ad392099d
Azamat Kenjebayev <azamatk@me.com>
2023-02-25 23:04:59 +0600

    initial commit

---
+++ Split The Bill/Split The BillUITests/Split_The_BillUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Split_The_BillUITestsLaunchTests.swift
+//  Split The BillUITests
+//
+//  Created by Azamat Kenjebayev on 2/25/23.
+//
+
+import XCTest
+
+final class Split_The_BillUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
